a5050ea953015eb82c50cd078ed19989e5626863
Puspraj Magar <pusprajmagar@gmail.com>
2026-07-22 21:31:36 +0530

    Remove backend from portfolio repository

--- portfolio-backend/routes/contact.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { handleContact } = require("../controllers/contact.controller");
-
-const router = express.Router();
-
-// Route: POST /api/contact (note the "/api" prefix is added in server.js)
-router.post("/contact", handleContact);
-
-module.exports = router;
